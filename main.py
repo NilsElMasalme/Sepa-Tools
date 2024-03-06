c3d63e7e3291da61c5f70c1d1e0e9762ca3c41a7
= <nils.masalme00@gmail.com>
2024-03-06 15:27:51 +0100

    for loop

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+from time import sleep
 
-db = SQLAlchemy()
-
-class Payment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Float, nullable=False)
-    description = db.Column(db.String(255), nullable=False)
-
-
-    def __init__(self, amount, description):
-        self.amount = amount
-        self.description = description
-
-    def __repr__(self):
-        return f'<Payment {self.id}>'
+for i in range(10):
+    sleep(1)
+    i += 1
+    print(i)
